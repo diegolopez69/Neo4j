7b60f461860890a96b6a79e435e27591b67679af
Diego López <diegolopezselva@gmail.com>
2021-05-28 13:56:31 +0200

    Adding the correct format of the code

--- backend/controllers/competicionesController.js
+++ backend/controllers/competicionesController.js
@@ -1,79 +1,94 @@
-let neo4j = require('neo4j-driver');
-const rabbitPublisher = require('../services/rabbit.publisher.service');
-let driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'test'));
+let neo4j = require("neo4j-driver");
+const rabbitPublisher = require("../services/rabbit.publisher.service");
+let driver = neo4j.driver(
+  "bolt://localhost",
+  neo4j.auth.basic("neo4j", "test")
+);
 
 module.exports = {
-    //Añadir una competición
-    add: (req, res) => {
-        let nombreCompeticion = req.body.nombre;
-        let session3 = driver.session();
-        session3
-            .run('CREATE(n:competicion {nombre:{nombreCompeticionParam}}) RETURN n.nombre', { nombreCompeticionParam: nombreCompeticion })
-            .then(function (result) {
-                rabbitPublisher.publishMessage('Competición añadida');
-                session3.close();
-                //res.redirect('/');
+  //Añadir una competición
+  add: (req, res) => {
+    let nombreCompeticion = req.body.nombre;
+    let session3 = driver.session();
+    session3
+      .run(
+        "CREATE(n:competicion {nombre:{nombreCompeticionParam}}) RETURN n.nombre",
+        { nombreCompeticionParam: nombreCompeticion }
+      )
+      .then(function (result) {
+        rabbitPublisher.publishMessage("Competición añadida");
+        session3.close();
+        //res.redirect('/');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
 
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+    res.redirect("/");
+  },
 
-        res.redirect('/');
-    },
-
-    //Buscar todas las competiciones
-    get:getCompeticiones = async (req, res) => {
-        let temp = [];
-        try {
-        const tempSession = driver.session();
-        const {records: data} = await tempSession.run('MATCH(n:competicion) RETURN n');
-        rabbitPublisher.publishMessage('competición añadida');
-        temp = data.map((record) => {
-            return {
-                id: record._fields[0].identity.low,
-                nombre: record._fields[0].properties.nombre
-            };
-        });
-        res.send({competiciones: temp});
-        } catch (exception) {
-            console.log("fallo por esto: ", exception)
+  //Buscar todas las competiciones
+  get: (getCompeticiones = async (req, res) => {
+    let temp = [];
+    try {
+      const tempSession = driver.session();
+      const { records: data } = await tempSession.run(
+        "MATCH(n:competicion) RETURN n"
+      );
+      rabbitPublisher.publishMessage("competición añadida");
+      temp = data.map((record) => {
+        return {
+          id: record._fields[0].identity.low,
+          nombre: record._fields[0].properties.nombre,
+        };
+      });
+      res.send({ competiciones: temp });
+    } catch (exception) {
+      console.log("fallo por esto: ", exception);
+    }
+    return temp;
+  }),
+  //Añadir una competición a un equipo
+  addEquipo: (req, res) => {
+    let session4 = driver.session();
+    let nombreCompeticion = req.body.nombreCompeticion;
+    let nombreEquipo = req.body.nombreEquipo;
+    session4
+      .run(
+        "MATCH(a:equipo {nombre: $nombreEquipoParam}), (b:competicion{nombre: $nombreCompeticionParam}) MERGE (a)-[r:COMPITE]-(b) RETURN a,b",
+        {
+          nombreEquipoParam: nombreEquipo,
+          nombreCompeticionParam: nombreCompeticion,
         }
-        return temp;
-    },
-    //Añadir una competición a un equipo
-    addEquipo: (req, res) => {
-        let session4 = driver.session();
-        let nombreCompeticion = req.body.nombreCompeticion;
-        let nombreEquipo = req.body.nombreEquipo;
-        session4
-            .run('MATCH(a:equipo {nombre: $nombreEquipoParam}), (b:competicion{nombre: $nombreCompeticionParam}) MERGE (a)-[r:COMPITE]-(b) RETURN a,b', { nombreEquipoParam: nombreEquipo, nombreCompeticionParam: nombreCompeticion })
-            .then(function (result) {
-                session4.close();
-                rabbitPublisher.publishMessage('Equipo añadido a una competición');
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-        res.redirect('/');
-    },
+      )
+      .then(function (result) {
+        session4.close();
+        rabbitPublisher.publishMessage("Equipo añadido a una competición");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    res.redirect("/");
+  },
 
-    //Eliminar una competición
-    delete: (req, res)=>{  
-        let nombreCompeticion = req.body.nombre;
-        let session8 = driver.session();
-        session8
-            .run('MATCH(n:competicion {nombre: $nombreCompeticionParam}) DETACH DELETE n', {nombreCompeticionParam:nombreCompeticion})
-            .then(function(result){
-                rabbitPublisher.publishMessage('competición eliminado');
-                session8.close();
-                //res.redirect('/');
-                
-            })
-            .catch(function(err){
-                console.log(err);
-            });
-    
-        res.redirect('/');
-    },
-}
+  //Eliminar una competición
+  delete: (req, res) => {
+    let nombreCompeticion = req.body.nombre;
+    let session8 = driver.session();
+    session8
+      .run(
+        "MATCH(n:competicion {nombre: $nombreCompeticionParam}) DETACH DELETE n",
+        { nombreCompeticionParam: nombreCompeticion }
+      )
+      .then(function (result) {
+        rabbitPublisher.publishMessage("competición eliminado");
+        session8.close();
+        //res.redirect('/');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+    res.redirect("/");
+  },
+};
